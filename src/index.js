a339a91fcefdf53429b55cd62457657c81a7fdad
shiv <shivkrrana@gmail.com>
2024-03-02 13:23:17 +0530

    corrected

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const cartRouter = require("./cartRouter.js");
 const taskRouter = require("./taskRouter.js");
 const userRouter = require("./userRouter.js");
-const productRouter = require("./productRouter.js");
 const cors = require("cors");
 var cookieParser = require('cookie-parser');
 require('dotenv').config();
@@ -11,10 +9,8 @@ require("./conn");
 const app = express();
 app.use(cookieParser());
 app.use(cors());
-app.use(cartRouter);
 app.use(taskRouter);
 app.use(userRouter);
-app.use(productRouter);
 
 const port = process.env.PORT || 5000
 
